f4b1b2a049e101a7a86b86beaa0981a5b385eb6c
ofest <dars20@hotmail.com>
2025-06-17 11:50:15 +0000

    Assign a random number each time it is executed. Number could be positive or negative

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -1,16 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-/*
+
+/**
  * main - Entry point
- *
+ * Description : assign a random number to know if is positive or negative.
  * Return: Always 0 (Success)
  */
+
 int main(void)
 
 {
 int n;
-
 srand(time(0));
 n = rand() - RAND_MAX / 2;
 if (n > 0)
