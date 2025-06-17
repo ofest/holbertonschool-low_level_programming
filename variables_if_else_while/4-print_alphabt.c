c2f7fab7c96603a5f8537b1e8d66663e06083630
ofest <dars20@hotmail.com>
2025-06-17 20:51:36 +0000

    need fix

---
+++ variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Entry point
+ * Description : Prints the alphabet in lowercase excluding 'q' and 'e'.
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char i;
+
+	for (i = 97 ; i <= 122 ; i++)
+		putchar(i);
+	if (!i = 101 ; !i = 103)
+		putchar(i);
+	putchar ('\n');
+
+	return (0);
+}
