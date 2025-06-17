936a18426a9f55512d45515bced3229f9d3dd114
ofest <dars20@hotmail.com>
2025-06-17 19:22:29 +0000

    An .exe that prints the alphabet in lowercase and UPPERCASE using 'ASCII table' and only using putchar command three times

---
+++ variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Entry point
+ * Description : Prints the alphabet in lowercase.
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char i;
+
+	for (i = 97 ; i <= 122 ; i++)
+		putchar(i);
+
+	for (i = 65 ; i <= 90 ; i++)
+		putchar(i);
+
+	putchar ('\n');
+
+	return (0);
+}
