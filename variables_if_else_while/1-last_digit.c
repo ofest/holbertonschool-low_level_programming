482d33f88225be756d8a6e046b0c595acb67ccb0
ofest <dars20@hotmail.com>
2025-06-17 14:01:25 +0000

    Assign a random number to print only the last digit of the number give

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -18,6 +18,7 @@ n = rand() - RAND_MAX / 2;
 
 if (n % 10 > 5)
 printf("Last digit of %d is %d and is greater than  5\n", n, n % 10);
-
+else if (n % 10 == 0)
+printf("Last digit of %d is %d and is 0\n", n, n % 10);
 return (0);
 }
