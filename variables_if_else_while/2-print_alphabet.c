3716b0ca37047fff7c1eee80125a72685403b313
ofest <dars20@hotmail.com>
2025-06-17 14:26:17 +0000

    Add and only with putchar to prints the alphabet in lower case

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Entry point
+ * Description : Prints the alphabet in lowercase.
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int n;
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+        putchar
+
+
+
+
+
+
+
+return (0);
+}
