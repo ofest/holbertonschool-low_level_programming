0961d00a2193a65758c50f4f4d807dc254b89f14
ofest <dars20@hotmail.com>
2025-06-18 16:16:18 +0000

    Create a program that prints 'putchar' using only putchar function with a new line

---
+++ functions_nested_loops/0-putchar.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "main.c"
+/**
+*main - Entry point
+*Description : Prints _putchar whit a space.
+*Return: Always 0 (Success)
+*
+*/
+
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+
+	return (0);
+}
