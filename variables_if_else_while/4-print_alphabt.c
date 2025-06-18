187f3e4a5c6ea3f4cb07aa04494c9cd99082ea68
ofest <dars20@hotmail.com>
2025-06-18 08:58:50 +0000

    Using the unary operator 'NOT' to remove the letters 'q' and 'e' from the alphabet

--- variables_if_else_while/4-print_alphabt.c
+++ variables_if_else_while/4-print_alphabt.c
@@ -13,9 +13,10 @@ int main(void)
 	char i;
 
 	for (i = 97 ; i <= 122 ; i++)
+
+		if (i != 101 && i != 113)
 		putchar(i);
-	if (!i = 101 ; !i = 103)
-		putchar(i);
+
 	putchar ('\n');
 
 	return (0);
