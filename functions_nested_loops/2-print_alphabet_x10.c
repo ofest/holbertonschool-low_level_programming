20e8b5c51c741f104f33a6e58b145fe669c5878f
ofest <dars20@hotmail.com>
2025-06-19 11:53:40 +0000

    fix

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ *  print_alphabet - Function to print alphabet
+ *  Description : function that prints the alphabet lowercase.
+ *  Return: Void.
+ */
+
+void print_alphabet(void)
+{
+	char i;
+
+	for (i = 97 ; i <= 122 ; i++)
+	_putchar(i);
+
+	_putchar ('\n');
+}
