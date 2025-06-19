5bd98b26ad8451ab52869da9855d300b1f55e980
ofest <dars20@hotmail.com>
2025-06-19 16:13:17 +0200

    fix

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -1,20 +1,22 @@
-#include <stdio.h>
 #include "main.h"
 
 /**
- *  print_alphabet - Function to print alphabet
+ *  print_alphabet_x10 - Function to print alphabet ten times.
  *  Description : function that prints the alphabet lowercase ten times.
  *  Return: Void.
  */
 
-void print_alphabet_x10(void);)
+void print_alphabet_x10(void)
 {
 	char i;
-	int i = 0;
+	int c = 0;
 
-	for(i < 10)
+	while (c <= 9)
+	{
 	for (i = 97 ; i <= 122 ; i++)
 	_putchar(i);
+	}
+	_putchar ('\n');\
 
-	_putchar ('\n');
+	c++;
 }
