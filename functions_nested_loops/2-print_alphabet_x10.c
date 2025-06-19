1b34633caed7c44f15074fc669e7874c0dd3c281
ofest <dars20@hotmail.com>
2025-06-19 16:51:39 +0200

    function that prints the alphabet 10 times using the 'while' loop

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -13,10 +13,11 @@ void print_alphabet_x10(void)
 
 	while (c <= 9)
 	{
-	for (i = 97 ; i <= 122 ; i++)
-	_putchar(i);
-	}
+		for (i = 97 ; i <= 122 ; i++)
+		{
+		_putchar(i);
+		}
 	_putchar ('\n');\
-
 	c++;
+	}
 }
