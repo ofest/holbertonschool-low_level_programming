0945f9177b07b3d291cc23d7d13aeef13f332ce3
ofest <dars20@hotmail.com>
2025-06-20 09:28:17 +0200

    Function to check if is a lowercaes or uppercase of the alphabet and print, using the ASCII table 97-122 (lowercase) and 65-90 (uppercase).

---
+++ functions_nested_loops/4-isalpha.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ *  _islalpha - Check lowercase or the uppercase character.
+ *  Description : function that prints the alphabet lowercase and uppercase.
+ *  @c : number to be checked.
+ *  Return: to 1 if lowercase, if not return 0
+ */
+
+int _isalpha(int c)
+{
+	if ((c >= 97 && c <= 122) || (c >= 65 && c <= 90))
+	{
+		return (1);
+	}
+	return (0);
+}
