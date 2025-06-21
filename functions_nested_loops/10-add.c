e69178d6d5e347ef5350a0dc32e5bdd005814c57
ofest <dars20@hotmail.com>
2025-06-21 22:23:19 +0200

    function that add two integers and return the result using the prototypes

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * add - adds two numbers together.
+ * @a: number 1
+ * @b: number 2
+ * Returns: sum of the two numbers.
+*/
+int add(int a, int b)
+{
+return (a + b);
+}
