eb30283d03f95b11d108fbf703d3eb76b5931e82
ofest <dars20@hotmail.com>
2025-06-24 14:08:39 +0200

    Program that prints a triangle of #

---
+++ more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+* print_triangle - Entry point of the program
+* @size: he height of the triangle to be printed.
+* Description: This function prints a triangle of hashtags (#)
+* If the size is less than or equal to zero, it prints a new line.
+*/
+void print_triangle(int size)
+{
+	int a, b, space;
+
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (a = 0; a < size; a++)
+		{
+			for (space = 0; space < size - a - 1; space++)
+				_putchar(' ');
+
+			for (b = a + 1; b > 0; b--)
+			_putchar('#');
+			_putchar('\n');
+		}
+	}
+}
