a55a06d811d09ed607d2a55343aa90fe9bbc825d
ofest <dars20@hotmail.com>
2025-06-26 15:34:00 +0200

    Add a function that prints half of the string

---
+++ pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * puts_half - Function that prints half of the string.
+ * @str: string.
+ */
+
+void puts_half(char *str)
+{
+	int n, length, mid;
+
+	length = 0;
+			
+	while (str[length] != '\0')
+ 	{
+	        length++;
+	}
+
+	if (length % 2  == 0)
+	{
+		mid = length / 2;
+		for (n = mid; n < length; n++)
+		{
+			_putchar(str[n]);
+		}
+	}
+	_putchar('\n');
+}
