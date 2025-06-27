1807080d970033fe6e7935105c5e6c888cc3c397
ofest <dars20@hotmail.com>
2025-06-27 09:49:00 +0200

    ADd a function that copies the string pointed to the buffer pointed

---
+++ pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _strcpy - Copies a string from source to destination.
+ * @dest: Pointer to the destination buffer.
+ * @src: Pointer to the source string.
+ *
+ * Return: Pointer to the destination buffer.
+ */
+
+char *_strcpy(char *dest, char *src)
+
+{
+	char *start = dest;
+
+	while ((*dest++ = *src++) != '\0');
+
+	return (start);
+}
