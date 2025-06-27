3be9ddc3395c591596c6ea9044e696dc2c02f233
ofest <dars20@hotmail.com>
2025-06-27 11:02:51 +0200

    Add a function that convert a string to an integer

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -3,41 +3,31 @@
 
 /**
  * _atoi - Copies a string from source to destination.
- * @result: Pointer to the destination buffer.
- * @sign: Pointer to the source string.
- * @i: 
- * Return: Pointer to the destination buffer.
+ * @s: array of sting
+ * Return: Convert integer or 0 if he doesnt find a number.
  */
 
 int _atoi(char *s)
+
 {
-    int result = 0;
-    int sign = 1;
-    int i = 0;
+	int i = 0, sign = 1, n = 0;
+	int found_num = 0;
 
+	while (s[i] != '\0')
+	{
+		if (s[i] == '-')
+			sign *= -1;
 
-    while
-	    (s[i] == ' ') 
-    {
-        i++;
-    }
-
-    while
-	    (s[i] == '-' || s[i] == '+') 
-    {
-        if (s[i] == '-')
-            sign *= -1;
-        i++;
-    }
-
-
-    while
-	    (s[i] >= '0' && s[i] <= '9') 
-    {
-        result = result * 10 + (s[i] - '0');
-	i++;
-    }
-
-    return result * sign;
+		else if (s[i] >= '0' && s[i] <= '9')
+		{
+			n = n * 10 + (s[i] - '0');
+			found_num = 1;
+		}
+		else if (found_num)
+		{
+			return (n * sign);
+		}
+		i++;
+	}
+	return (n * sign);
 }
-
