e120307629e979675c5bd1a99bf6a14c1b61dc48
ofest <dars20@hotmail.com>
2025-06-28 21:34:35 +0200

    Add a functions that compares two strings, taking the ASCII valors

---
+++ pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _strcmp - Function that compares two strings.
+ * @s2: String one.
+ * @s1: String two.
+ * Return: Return the valors ASCII
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+	int i = 0;
+
+	while (s1[i] != '\0' && s2[i] != '\0' && s1[i] == s2[i])
+	{
+	i++;
+	}
+	return (s1[i] - s2[i]);
+}
