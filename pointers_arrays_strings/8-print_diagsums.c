de8cf3191d76487d5989e4ef98a4d36808a1611f
ofest <dars20@hotmail.com>
2025-07-01 15:06:36 +0200

    Add a function that prints the sum of two diagonals of a square matrix of integers

---
+++ pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_diagsums - Function that prints a chessboard.
+ * @a: String with the board.
+ * @size: size ofe
+ */
+
+void print_diagsums(int *a, int size)
+
+{
+	int row;
+	int sum_primary = 0;
+	int sum_secondary = 0;
+
+        for (row = 0; row < size; row++)
+	{
+		sum_primary += a[row * size + row];
+		sum_secondary += a[row * size + (size - 1 - row)];
+	}
+	printf("%d, %d\n", sum_primary, sum_secondary);
+}
