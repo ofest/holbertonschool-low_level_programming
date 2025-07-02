7e6e18002d1d2e1f8921dab93eb2d95de4d1a6dd
ofest <dars20@hotmail.com>
2025-07-02 10:31:15 +0200

    Add a function that give the value x raised with y

--- recursion/4-pow_recursion.c
+++ recursion/4-pow_recursion.c
@@ -5,7 +5,7 @@
  *  _pow_recursion - returns the value of x raised to the power of y.
  * @y: Number.
  * @x: Nunber.
- * Return: y<0 return -1, y=+0 return 1, if.
+ * Return: y<0 return -1, y==0 return 1, if.
  */
 
 int _pow_recursion(int x, int y)
