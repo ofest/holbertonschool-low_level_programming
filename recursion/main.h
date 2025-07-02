53c81736d417d06201a5b83baee821ecee94013b
ofest <dars20@hotmail.com>
2025-07-02 16:27:41 +0200

    Prototypes

--- recursion/main.h
+++ recursion/main.h
@@ -6,3 +6,5 @@ int factorial(int n);
 int _pow_recursion(int x, int y);
 int _sqrt_recursion(int n);
 int sqrt_helper(int n, int g);	
+int is_prime_number(int n);
+int prime_helper(int n, int divisor);
