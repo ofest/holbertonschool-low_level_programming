08e26b9b58f71ead3091cec777ae1f14dec9ee1d
ofest <dars20@hotmail.com>
2025-07-03 19:32:41 +0200

    Add a function that prints all arguments

--- argc_argv/2-args.c
+++ argc_argv/2-args.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 
 /**
- * main - Program that prints its name.
+ * main - Program that prints all arguments.
  * @argc: The argument count and represents the number.
  * @argv: Array of strings.
  * Return: Return 0.
