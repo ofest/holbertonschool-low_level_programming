59f2c56e132db53b17479ef8f4723cbadef36bf3
ofest <dars20@hotmail.com>
2025-07-03 19:30:59 +0200

    Add a function that prints all arguments

---
+++ argc_argv/2-args.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Program that prints its name.
+ * @argc: The argument count and represents the number.
+ * @argv: Array of strings.
+ * Return: Return 0.
+ */
+
+int main(int argc, char *argv[])
+
+{
+	int n = 0;
+
+	for (n = 0; n < argc; n++)
+	{
+		printf("%s\n", argv[n]);
+	}
+	return (0);
+}
