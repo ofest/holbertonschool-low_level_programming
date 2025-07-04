71f27f0be631766f84ed80fd699fe990b930b559
ofest <dars20@hotmail.com>
2025-07-04 11:29:07 +0200

    Add aprogram that multiplicates two numbers, using 'atoi' to convert the strings to integers

--- argc_argv/3-mul.c
+++ argc_argv/3-mul.c
@@ -11,17 +11,21 @@
 int main(int argc, char *argv[])
 
 {
-	int a = atoi(argv[1]);
-	int b = atoi(argv[2]);
+	int a;
+        int b;
+        int result;
 
-	int result = a * b;
-	{
-	printf("%d\n", result);
-	}
 	if (argc != 3)
-	{
-		printf("Error\n");
-		return (1);
-	}
+        {
+                printf("Error\n");
+                return (1);
+        }
+	
+	a = atoi(argv[1]);
+	b = atoi(argv[2]);
+
+	result = a * b;
+	printf("%d\n", result);
 	return (0);
+
 }
