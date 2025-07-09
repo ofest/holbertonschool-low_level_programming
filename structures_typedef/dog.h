6c49fa9f2797c0235c34f9de94d5a19ca0fc3022
ofest <dars20@hotmail.com>
2025-07-09 15:21:52 +0200

    Add a function that save the information given using 'struc' and 'typedef'.

---
+++ structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * typedef struc dog - Fuction to save the information given.
+ * @age: Age of the dog
+ * @owner: Name of the owner.
+ * @name: Name of the dog.
+ */
+
+typedef struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+} dog;
+
+
