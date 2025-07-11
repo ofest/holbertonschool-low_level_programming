776bd66c7fc2d58123a8b53a17cf84c755f29ea0
ofest <dars20@hotmail.com>
2025-07-11 16:01:36 +0200

    Add a new 'dog'

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include "dog.h"
 #include <stdlib.h>
-
+#include <string.h>
 /**
  * new_dog - Creates a new dog.
  * @name: pointer to name of dog
@@ -9,49 +9,37 @@
  * @owner: pointer to owner of dog
  * Return: Always 0.
  */
-
 dog_t *new_dog(char *name, float age, char *owner)
 {
-char *name_copy, *owner_copy;
-int i, k;
-dog_t *new_dog;
+dog_t *dogy;
 
-new_dog = malloc(sizeof(dog_t));
-
-if (new_dog == NULL)
+dogy = malloc(sizeof(dog_t));
+if (dogy == NULL)
 return (NULL);
+
 if (name != NULL)
 {
-for (i = 0; *(name + i) != '\0'; i++)
-;
-
-name_copy = malloc(sizeof(char) * (i + 1));
-if (name_copy == NULL)
+dogy->name = malloc(strlen(name) + 1);
+if (dogy->name == NULL)
 {
-free(new_dog);
+free(dogy);
 return (NULL);
 }
-
-for (k = 0; k <= i; k++)
-name_copy[k] = *(name + k);
-new_dog->name = name_copy;
+strcpy(dogy->name, name);
 }
 if (owner != NULL)
 {
-for (i = 0; *(owner + i) != '\0'; i++)
-;
-owner_copy = malloc(sizeof(char) * (i + 1));
-if (owner_copy == NULL)
+dogy->owner = malloc(strlen(owner) + 1);
+if (dogy->owner == NULL)
 {
-free(name_copy);
-free(new_dog);
+free(dogy->name);
+free(dogy);
 return (NULL);
 }
-for (k = 0; k <= i; k++)
-owner_copy[k] = *(owner + k);
-new_dog->owner = owner_copy;
+strcpy(dogy->owner, owner);
 }
 if (age >= 0)
-new_dog->age = age;
-return (new_dog);
+dogy->age = age;
+
+return (dogy);
 }
