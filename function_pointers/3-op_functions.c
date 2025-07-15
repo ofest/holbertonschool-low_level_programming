1984456f7a068d8d68dffeeba29682e27ace127e
ofest <dars20@hotmail.com>
2025-07-15 15:58:03 +0200

    Add the differents operations.

--- function_pointers/3-op_functions.c
+++ function_pointers/3-op_functions.c
@@ -49,7 +49,7 @@ int op_div(int a, int b)
 {
 if (b == 0)
 	{
-	return (-1);
+	return (0);
 	}
 return (a / b);
 }
@@ -65,7 +65,7 @@ int op_mod(int a, int b)
 {
 if (b == 0)
 	{
-	return (-1);
+	return (0);
 	}
 return (a % b);
 }
