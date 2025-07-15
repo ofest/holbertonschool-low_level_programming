09a5e37923800658233431416beb84bd11b63e1a
ofest <dars20@hotmail.com>
2025-07-15 16:16:56 +0200

    This file should contain the function that selects the correct function to perform the operation asked by the user.

--- function_pointers/3-get_op_func.c
+++ function_pointers/3-get_op_func.c
@@ -3,6 +3,13 @@
 #include <stdlib.h>
 #include <string.h>
 
+/**
+ * get_op_func - Add a function that determines the correct function.
+ *
+ * @s: operator to match to a function
+ * Return: pointer.
+ */
+
 int (*get_op_func(char *s))(int, int)
 {
 op_t ops[] = {
