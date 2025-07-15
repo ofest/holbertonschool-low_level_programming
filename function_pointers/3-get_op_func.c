fd10126030b9e73d8d8ebf47e14a4e28d74217bc
ofest <dars20@hotmail.com>
2025-07-15 14:16:08 +0200

    This file should contain the function that selects the correct function to perform the operation asked by the user.

---
+++ function_pointers/3-get_op_func.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include "3-calc.h"
+
+int (*get_op_func(char *s))(int, int)
+{
+op_t ops[] = 
+{
+{"+", op_add},
+{"-", op_sub},
+{"*", op_mul},
+{"/", op_div},
+{"%", op_mod},
+{NULL, NULL}
+};
+int i;
+
+i = 0;
+
+while (ops[i].s != NULL)
+{
+	if(strcmp(ops[i].s, s) == 0)
+	return(ops[i].f);
+	i++;
+}
+
+return (NULL);
+}
