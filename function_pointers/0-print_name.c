c4d4d8eb07bf9a9240f2474bf27ed64544e2cd1b
ofest <dars20@hotmail.com>
2025-07-15 14:32:29 +0200

    Add a function that prints a name.

--- function_pointers/0-print_name.c
+++ function_pointers/0-print_name.c
@@ -10,18 +10,12 @@
  */
 
 void print_name(char *name, void (*f)(char *))
-
 {
-if (name == NULL)
-{
-name = malloc(sizeof(char *));
-if (name == NULL)
+if (name == NULL || f == NULL)
 {
 return;
 }
-}
 
-if (f == NULL)
 f(name);
 return;
 
