008c26efd9a4701358c111af1d323ed178750236
ofest <dars20@hotmail.com>
2025-07-16 10:49:05 +0200

    Add a function that sums all the numbers given

---
+++ variadic_functions/0-sum_them_all.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include "variadic_functions.h"
+#include <stdarg.h>
+
+/**
+* sum_them_all - Sum all the parameters.
+* @n: Numbersto sum.
+* Return: sum.
+*/
+
+int sum_them_all(const unsigned int n, ...)
+{
+unsigned int i;
+int sum = 0;
+int value;
+va_list list;
+
+if (n == 0)
+	{
+	return (0);
+	}
+
+va_start(list, n);
+
+for (i = 0; i < n; i++)
+	{
+	value = va_arg(list, int);
+	sum += value;
+	}
+va_end(list);
+return (sum);
+}
