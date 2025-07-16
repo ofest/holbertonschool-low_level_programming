062085d7b8111a082847515147f8c4707a1917f1
ofest <dars20@hotmail.com>
2025-07-16 10:58:46 +0200

    Prototypes

--- variadic_functions/variadic_functions.h
+++ variadic_functions/variadic_functions.h
@@ -1,4 +1,9 @@
+#ifndef variadic_functions
+#define variadic_functions
+
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
+
+#endif /* variadic_functions */
