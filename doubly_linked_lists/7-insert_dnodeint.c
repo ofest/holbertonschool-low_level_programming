010d0ec90718cbab53da95f918cbd0ee7c4f82b3
ofest <dars20@hotmail.com>
2025-08-01 15:06:11 +0200

    Add a function that inserts a new node at a given position

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -10,43 +10,36 @@
 
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-	dlistint_t *new_node;
-	dlistint_t *current = *h;
-	unsigned int count = 0;
 
-	if (h == NULL)
-		return (NULL);
-	new_node = malloc(sizeof(dlistint_t));
-	if (new_node == NULL)
-	{
-		return (NULL);
-	}
-	new_node->n = n;
+	unsigned int i;
+	dlistint_t *tmp_node;
+	dlistint_t *new_node;
+
+	tmp_node = *h;
 	if (idx == 0)
 	{
-		new_node->next = current;
-		new_node->prev = NULL;
-		if (current != NULL)
-		{
-			current->prev = new_node;
-		}
-	*h = new_node;
-	return (new_node);
+		tmp_node = add_dnodeint(h, n);
+		return (tmp_node);
 	}
-	while (current != NULL && count < idx)
+
+	i = 1;
+	while (i < idx)
 	{
-		count++;
-		if (count == idx)
-		{
-			new_node->prev = current->prev;
-			new_node->next = current;
-			if (current->prev != NULL)
-				current->prev->next = new_node;
-			current->prev = new_node;
-			return (new_node);
-		}
-		current = current->next;
+		tmp_node = tmp_node->next;
+		if (tmp_node == NULL)
+			return (NULL);
+		i++;
 	}
-	free(new_node);
-	return (NULL);
+
+	if (tmp_node->next == NULL)
+		return (add_dnodeint_end(h, n));
+	new_node = malloc(sizeof(dlistint_t));
+	if (new_node == NULL)
+		return (NULL);
+	new_node->n = n;
+	new_node->prev = tmp_node;
+	new_node->next = tmp_node->next;
+	tmp_node->next->prev = new_node;
+	tmp_node->next = new_node;
+	return (new_node);
 }
