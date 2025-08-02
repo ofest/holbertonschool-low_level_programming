a571a4e475f9e877d92cdbdc8fbe409e682bfd85
ofest <dars20@hotmail.com>
2025-08-02 10:15:34 +0200

    Prototypes and structures

--- hash_tables/hash_tables.h
+++ hash_tables/hash_tables.h
@@ -4,7 +4,6 @@
 #include <stdlib.h>
 #include <string.h>
 #include <stdio.h>
-#include "hash_tables.h"
 
 /**
  * struct hash_node_s - Node of a hash table
@@ -16,9 +15,9 @@
  */
 typedef struct hash_node_s
 {
-     char *key;
-     char *value;
-     struct hash_node_s *next;
+	char *key;
+	char *value;
+	struct hash_node_s *next;
 } hash_node_t;
 
 /**
@@ -31,8 +30,8 @@ typedef struct hash_node_s
  */
 typedef struct hash_table_s
 {
-     unsigned long int size;
-     hash_node_t **array;
+	unsigned long int size;
+	hash_node_t **array;
 } hash_table_t;
 
 hash_table_t *hash_table_create(unsigned long int size);
