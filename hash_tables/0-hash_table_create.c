75386991ef49db163282cac38538c3c2bff39b71
ofest <dars20@hotmail.com>
2025-08-02 10:12:58 +0200

    Fix a function that creates a hash table

--- hash_tables/0-hash_table_create.c
+++ hash_tables/0-hash_table_create.c
@@ -11,24 +11,17 @@
 
 hash_table_t *hash_table_create(unsigned long int size)
 {
-unsigned long int i;
-hash_table_t *ht;
+	hash_table_t *hash_table;
+	unsigned long int i;
 
-ht = malloc(sizeof(hash_table_t));
-	if (ht == NULL)
-	{
+	hash_table = malloc(sizeof(hash_table_t));
+	if (hash_table == NULL)
 		return (NULL);
-	}
-ht->size = size;
-ht->array = malloc(sizeof(struct hash_node_t *) * size);
-
-	if (ht->array == NULL)
-	{
+	hash_table->size = size;
+	hash_table->array = malloc(sizeof(hash_node_t *) * size);
+	if (hash_table->array == NULL)
 		return (NULL);
-	}
 	for (i = 0; i < size; i++)
-	{
-			ht->array[i] = NULL;
-	}
-return (ht);
+		hash_table->array[i] = NULL;
+	return (hash_table);
 }
